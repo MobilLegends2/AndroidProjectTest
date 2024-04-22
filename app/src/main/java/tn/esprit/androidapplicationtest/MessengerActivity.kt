dd7abb0a62bd523d4d0290e6743772e3df0059d8
ademseddik <adem.seddik@esprit.tn>
2024-04-22 23:51:03 +0100

    correction

--- app/src/main/java/tn/esprit/androidapplicationtest/MessengerActivity.kt
+++ app/src/main/java/tn/esprit/androidapplicationtest/MessengerActivity.kt
@@ -6,16 +6,19 @@ import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
 import android.util.Log
+import android.view.Window
 import android.widget.EditText
 import android.widget.ImageView
 import android.widget.LinearLayout
 import android.widget.ScrollView
+import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+import com.google.android.material.bottomsheet.BottomSheetDialog
 import io.socket.client.IO
 import io.socket.client.Socket
 import kotlinx.coroutines.CoroutineScope
@@ -101,16 +104,25 @@ class MessengerActivity : AppCompatActivity() {
 
 // Retrieve the conversation ID from intent or wherever you get it
         conversationId = intent.getStringExtra("conversationId") ?: ""
+        var sendername =intent.getStringExtra("SENDERNAME") ?: ""
         if (conversationId.isBlank()) {
             // Handle the case where conversationId is not available
             return
         }
 
+        var sendernameui =findViewById<TextView>(R.id.personname)
         val sendButton = findViewById<ImageView>(R.id.plus)
+        val moreoption=findViewById<ImageView>(R.id.addd)
         val videoBtn = findViewById<ImageView>(R.id.videoBtn)
         val messageEditText = findViewById<EditText>(R.id.editTextUsername)
-        val attachmentButton = findViewById<ImageView>(R.id.voicemessage)
+
+        moreoption.setOnClickListener{
+            showMoreOptionDialog()
+        }
+
+//       // val attachmentButton = findViewById<ImageView>(R.id.voicemessage)
         val attachmentDisplayButton = findViewById<ImageView>(R.id.attachmentDisplayButton)
+        sendernameui.text=sendername
         attachmentDisplayButton.setOnClickListener {
             displayAttachments()
         }
@@ -134,12 +146,18 @@ class MessengerActivity : AppCompatActivity() {
                 refreshMessages()
             }
         }
-        attachmentButton.setOnClickListener {
-            val intent = Intent(Intent.ACTION_GET_CONTENT)
-            intent.type = "*/*"  // Allow any file type
-            startActivityForResult(intent, REQUEST_PICK_FILE)
-        }
 
+
+
+
+
+        /////////////////////////////////////////////
+//        attachmentButton.setOnClickListener {
+//            val intent = Intent(Intent.ACTION_GET_CONTENT)
+//            intent.type = "*/*"  // Allow any file type
+//            startActivityForResult(intent, REQUEST_PICK_FILE)
+//        }
+//////////////////////////////////////////////////////////
 
         // Initialize Retrofit and make network call
         val retrofit = Retrofit.Builder()
@@ -175,64 +193,75 @@ class MessengerActivity : AppCompatActivity() {
     }
     private fun sendAttachment(filePath: String, conversationId: String) {
         if (File(filePath).exists()) {
-        CoroutineScope(Dispatchers.IO).launch {
-            try {
-                val fileName = File(filePath).name
-                val url = URL("http://10.0.2.2:9090/conversations/$conversationId/attachments")
-                val connection = url.openConnection() as HttpURLConnection
-                connection.requestMethod = "POST"
-                connection.doOutput = true
-                connection.setRequestProperty("Content-Type", "multipart/form-data;boundary=*****")
+            CoroutineScope(Dispatchers.IO).launch {
+                try {
+                    val fileName = File(filePath).name
+                    val url = URL("http://10.0.2.2:9090/conversations/$conversationId/attachments")
+                    val connection = url.openConnection() as HttpURLConnection
+                    connection.requestMethod = "POST"
+                    connection.doOutput = true
+                    connection.setRequestProperty("Content-Type", "multipart/form-data;boundary=*****")
 
-                val outputStream = DataOutputStream(connection.outputStream)
-                val lineEnd = "\r\n"
-                val twoHyphens = "--"
-                val boundary = "*****"
+                    val outputStream = DataOutputStream(connection.outputStream)
+                    val lineEnd = "\r\n"
+                    val twoHyphens = "--"
+                    val boundary = "*****"
 
-                outputStream.writeBytes("$twoHyphens$boundary$lineEnd")
-                outputStream.writeBytes("Content-Disposition: form-data; name=\"file\";filename=\"$fileName\"$lineEnd")
-                outputStream.writeBytes(lineEnd)
+                    outputStream.writeBytes("$twoHyphens$boundary$lineEnd")
+                    outputStream.writeBytes("Content-Disposition: form-data; name=\"file\";filename=\"$fileName\"$lineEnd")
+                    outputStream.writeBytes(lineEnd)
 
-                val fileInputStream = FileInputStream(filePath)
-                var bytesAvailable = fileInputStream.available()
-                var bufferSize = Math.min(bytesAvailable, 1024)
-                val buffer = ByteArray(bufferSize)
-                var bytesRead = fileInputStream.read(buffer, 0, bufferSize)
+                    val fileInputStream = FileInputStream(filePath)
+                    var bytesAvailable = fileInputStream.available()
+                    var bufferSize = Math.min(bytesAvailable, 1024)
+                    val buffer = ByteArray(bufferSize)
+                    var bytesRead = fileInputStream.read(buffer, 0, bufferSize)
 
-                while (bytesRead > 0) {
-                    outputStream.write(buffer, 0, bufferSize)
-                    bytesAvailable = fileInputStream.available()
-                    bufferSize = Math.min(bytesAvailable, 1024)
-                    bytesRead = fileInputStream.read(buffer, 0, bufferSize)
+                    while (bytesRead > 0) {
+                        outputStream.write(buffer, 0, bufferSize)
+                        bytesAvailable = fileInputStream.available()
+                        bufferSize = Math.min(bytesAvailable, 1024)
+                        bytesRead = fileInputStream.read(buffer, 0, bufferSize)
+                    }
+
+                    outputStream.writeBytes(lineEnd)
+                    outputStream.writeBytes("$twoHyphens$boundary$twoHyphens$lineEnd")
+
+                    // Close streams
+                    fileInputStream.close()
+                    outputStream.flush()
+                    outputStream.close()
+
+                    val responseCode = connection.responseCode
+                    if (responseCode == HttpURLConnection.HTTP_CREATED) {
+                        // Attachment sent successfully
+                        Log.d("MessengerActivity", "Attachment sent successfully")
+                    } else {
+                        // Error sending attachment
+                        Log.e("MessengerActivity", "Failed to send attachment. Response code: $responseCode")
+                    }
+                    connection.disconnect()
+                } catch (e: Exception) {
+                    // Handle any errors that occur during the HTTP request
+                    Log.e("MessengerActivity", "Error sending attachment: ${e.message}")
                 }
-
-                outputStream.writeBytes(lineEnd)
-                outputStream.writeBytes("$twoHyphens$boundary$twoHyphens$lineEnd")
-
-                // Close streams
-                fileInputStream.close()
-                outputStream.flush()
-                outputStream.close()
-
-                val responseCode = connection.responseCode
-                if (responseCode == HttpURLConnection.HTTP_CREATED) {
-                    // Attachment sent successfully
-                    Log.d("MessengerActivity", "Attachment sent successfully")
-                } else {
-                    // Error sending attachment
-                    Log.e("MessengerActivity", "Failed to send attachment. Response code: $responseCode")
-                }
-                connection.disconnect()
-            } catch (e: Exception) {
-                // Handle any errors that occur during the HTTP request
-                Log.e("MessengerActivity", "Error sending attachment: ${e.message}")
             }
-        }
         } else {
             Log.e("MessengerActivity", "File does not exist at path: $filePath")
         }
     }
-
+    private fun showMoreOptionDialog() {
+        val dialog = BottomSheetDialog(this)
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE)
+        dialog.setContentView(R.layout.moreoptions)
+        var attachbutton = dialog.findViewById<ImageView>(R.id.attach)
+        attachbutton?.setOnClickListener {
+            val intent = Intent(Intent.ACTION_GET_CONTENT)
+            intent.type = "*/*"  // Allow any file type
+            startActivityForResult(intent, REQUEST_PICK_FILE)
+        }
+        dialog.show()
+    }
     private fun refreshMessages() {
         val retrofit = Retrofit.Builder()
             .baseUrl("http://10.0.2.2:9090/") // Update with your server URL
